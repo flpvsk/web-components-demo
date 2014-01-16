c73e4066c6868fa95e4af744fbc03ddcf68fa47f
filipovsky <filipovskii.off@gmail.com>
2014-01-16 18:19:52 +0400

    Custom Shadow DOM.

---
+++ wc-shadowdom-custom/index.js
@@ -0,0 +1,48 @@
+// ## [ГЛАВНАЯ](../..) | [ДЕМО](..)
+'.';
+
+
+(function () {
+
+  window.onload = function () {
+    var templates = document.querySelectorAll('template'),
+        shadowHost = document.querySelector('.shadow-host'),
+        contentEl = document.querySelector('.content'),
+        i, l;
+
+    // Для каждого шаблона...
+    for (i = 0, l = templates.length; i < l; i++) {
+      var template = templates.item(i),
+          templateBody = document.importNode(template.content),
+          shadowHostClone = shadowHost.cloneNode(true),
+          pEl = document.createElement('p'),
+          templatePreEl = document.createElement('pre'),
+          templateHeaderEl = document.createElement('h3'),
+          shadowRoot;
+
+      // Создаю <strong>Shadow Root</strong> для элемента,
+      // который будет содержать <strong>Shadow DOM</strong>.
+      // Сам элемент таким образом становится
+      // <strong>Shadow Host</strong>.
+      shadowRoot = shadowHostClone.createShadowRoot();
+
+      // Заполняю заголовок.
+      templateHeaderEl.innerText = (i + 1) + '. #' + template.id;
+      // Вставляю превью шаблона.
+      templatePreEl.innerText = template.innerHTML;
+
+      pEl.appendChild(templateHeaderEl);
+      pEl.appendChild(templatePreEl);
+
+      // Добавляю содержимое шаблона в <strong>Shadow DOM</strong> хоста.
+      shadowRoot.appendChild(templateBody);
+
+      // Добавляю описание шаблона на страницу.
+      contentEl.appendChild(pEl);
+      // И сам элемент, хост, туда же.
+      contentEl.appendChild(shadowHostClone);
+    }
+
+  };
+
+})();
