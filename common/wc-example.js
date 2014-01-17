3025fc702ec32daa5e2fcaae983d7d45f87eebe7
filipovsky <filipovskii.off@gmail.com>
2014-01-17 19:41:36 +0400

    Styles.

--- common/wc-example.js
+++ common/wc-example.js
@@ -41,98 +41,104 @@
     return lines.join('\n');
   };
 
-    console.log('herer1');
 
-    var wcExampleProto = Object.create(HTMLElement.prototype),
-        owner = document.currentScript.ownerDocument || document;
-
-
-    wcExampleProto.createdCallback = function () {
-      var tmpl = owner.querySelector('#wc-example-tmpl'),
-          tmplBody = document.importNode(tmpl.content, true),
-          markupHolder = tmplBody.querySelector('[data-code=markup]'),
-          markupEl = this.querySelector('wc-markup'),
-          jsHolder = tmplBody.querySelector('[data-code=js]'),
-          jsEl =  this.querySelector('wc-script'),
-          tmplHolder = tmplBody.querySelector('[data-code=tmpl]'),
-          tmplEl =  this.querySelector('template'),
-          shadow = this.createShadowRoot(),
-          previewHolder = tmplBody.querySelector('.preview'),
-          previewContent,
-          s, i, l;
-
-      if (markupEl) {
-        markupHolder.innerText = clearWS(markupEl.innerHTML);
-        hljs.highlightBlock(markupHolder);
-      }
-
-      if (jsEl) {
-        jsHolder.innerText = clearWS(jsEl.innerHTML);
-        hljs.highlightBlock(jsHolder);
-      }
-
-      if (tmplEl) {
-        s = tmplEl.outerHTML.split('\n')[0];
-        s = s + clearWS(tmplEl.innerHTML).split('\n').map(function (l) {
-          if (l.length === 0) { return l; }
-          return '  ' + l;
-        }).join('\n');
-        s = s + '</template>';
-        tmplHolder.innerText = s;
-        hljs.highlightBlock(tmplHolder);
-      }
-
-      shadow.appendChild(tmplBody);
-      shadow.applyAuthorStyles = true;
+  var wcExampleProto = Object.create(HTMLElement.prototype),
+      owner = document.currentScript.ownerDocument || document;
 
 
-      for (i = 0, l = markupEl.childElementCount; i < l; i++) {
-        previewHolder.appendChild(markupEl.children[i].cloneNode(true));
-      }
+  wcExampleProto.createdCallback = function () {
+    var tmpl = owner.querySelector('#wc-example-tmpl'),
+        tmplBody = document.importNode(tmpl.content, true),
+        markupHolder = tmplBody.querySelector('[data-code=markup]'),
+        markupEl = this.querySelector('wc-markup'),
+        jsHolder = tmplBody.querySelector('[data-code=js]'),
+        jsEl =  this.querySelector('wc-script'),
+        tmplHolder = tmplBody.querySelector('[data-code=tmpl]'),
+        tmplEl =  this.querySelector('template'),
+        shadow = this.createShadowRoot(),
+        previewHolder = tmplBody.querySelector('.preview'),
+        previewShadow,
+        s, i, l;
 
-      previewHolder
-        .querySelector('.shadow-host')
-        .createShadowRoot()
-        .appendChild(tmplEl.content);
-    };
+    if (markupEl) {
+      markupHolder.innerText = clearWS(markupEl.innerHTML);
+      hljs.highlightBlock(markupHolder);
+    }
 
-    console.log('herer');
-    /*
-    [ document, owner ].forEach(function (doc, i) {
+    if (jsEl) {
+      jsHolder.innerText = clearWS(jsEl.innerHTML);
+      hljs.highlightBlock(jsHolder);
+    }
 
-      if (!doc) {
-        console.warn('Document absent', i);
-      }
+    if (tmplEl) {
+      s = tmplEl.outerHTML.split('\n')[0];
+      s = s + clearWS(tmplEl.innerHTML).split('\n').map(function (l) {
+        if (l.length === 0) { return l; }
+        return '  ' + l;
+      }).join('\n');
+      s = s + '</template>';
+      tmplHolder.innerText = s;
+      hljs.highlightBlock(tmplHolder);
+    }
 
-      console.log('Registering');
-
-      doc.registerElement('wc-markup', {
-        prototype: Object.create(HTMLElement.prototype)
-      });
-
-      doc.registerElement('wc-script', {
-        prototype: Object.create(HTMLElement.prototype)
-      });
-
-      doc.registerElement('wc-example', {
-        prototype: wcExampleProto
-      });
-
-    });
-    */
+    shadow.appendChild(tmplBody);
+    shadow.applyAuthorStyles = true;
 
 
-    document.registerElement('wc-markup', {
+    for (i = 0, l = markupEl.childElementCount; i < l; i++) {
+      previewHolder.appendChild(markupEl.children[i].cloneNode(true));
+    }
+
+    previewShadow = previewHolder
+      .querySelector('.shadow-host')
+      .createShadowRoot();
+    previewShadow.appendChild(tmplEl.content);
+
+    if (this.dataset.hasOwnProperty('resetStyleInheritance')) {
+      previewShadow.resetStyleInheritance = true;
+    }
+
+    if (this.dataset.hasOwnProperty('applyAuthorStyles')) {
+      previewShadow.applyAuthorStyles = true;
+    }
+  };
+
+  /*
+  [ document, owner ].forEach(function (doc, i) {
+
+    if (!doc) {
+      console.warn('Document absent', i);
+    }
+
+    console.log('Registering');
+
+    doc.registerElement('wc-markup', {
       prototype: Object.create(HTMLElement.prototype)
     });
 
-    document.registerElement('wc-script', {
+    doc.registerElement('wc-script', {
       prototype: Object.create(HTMLElement.prototype)
     });
 
-    document.registerElement('wc-example', {
+    doc.registerElement('wc-example', {
       prototype: wcExampleProto
     });
+
+  });
+  */
+
+
+  document.registerElement('wc-markup', {
+    prototype: Object.create(HTMLElement.prototype)
+  });
+
+  document.registerElement('wc-script', {
+    prototype: Object.create(HTMLElement.prototype)
+  });
+
+  document.registerElement('wc-example', {
+    prototype: wcExampleProto
+  });
 
 
 })();
