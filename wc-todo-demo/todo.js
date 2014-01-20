d72323748758fbc6188157809f112493c24e256f
filipovsky <filipovskii.off@gmail.com>
2014-01-20 18:03:05 +0400

    TODO demo docs.

--- wc-todo-demo/todo.js
+++ wc-todo-demo/todo.js
@@ -1,23 +1,39 @@
+// # TODO Widget
+//
+// ## [ГЛАВНАЯ](../..) | [ДЕМО](..)
+//
+'.';
+
 (function () {
+
+  // Ссылка на документ с кодом виджета
   var owner = (document.currentScript || {}).ownerDocument || document,
+      // Задачи
       items = [],
+      // Прототип кастомного элемента наследует *HTMLElement*.
       todoProto = Object.create(HTMLElement.prototype, {
+
+        // Добавить задачу в список.
         addItem: {
           value: function (text) {
             items.push(text);
           }
         },
 
+        // Удалить задачу.
         removeItem: {
           value: function (id) {
             delete items[id];
           }
         },
 
+        // Получить список.
         items: {
           get: function () { return items.slice(); }
         },
 
+        // Callback будет вызван при добавлении &lt;x-todo&gt; элемента на
+        // страницу.
         createdCallback: {
           value: function () {
             var todos = this,
@@ -26,23 +42,33 @@
                 tmplBody = document.importNode(tmpl.content),
                 addBtn;
 
+            // Виджет не будет наследовать стили документа, в который
+            // встроен.
             shadow.resetStyleInheritance = true;
             shadow.appendChild(tmplBody);
 
             addBtn = shadow.querySelector('#add-todo');
             addBtn.addEventListener('click', function () {
               var newTask = shadow.querySelector('#new-task');
+
+              // В обработчике вызываю метод `добавить задачу`,
+              // определённый ранее.
               todos.addItem(newTask.value);
               newTask.value = '';
             });
 
 
+            // `Object.observe` вызывет переданный callback на каждое
+            // изменение `items`.
             Object.observe(items, function (changes) {
               var list = shadow.querySelector('#tasks');
 
               changes.forEach(function (change) {
                 var li;
 
+                // Если в список добавили задачу,
+                // создаю соответсвующий &lt;li&gt;,
+                // вешаю обработчик клика.
                 if (change.type === 'add') {
                   li = document.createElement('li');
                   li.setAttribute('data-id', change.name);
@@ -54,6 +80,7 @@
                   return;
                 }
 
+                // Если удалили, удаляю соответсвующий &lt;li&gt;.
                 if (change.type === 'delete') {
                   li = list.querySelector(
                     '[data-id="' + change.name + '"]');
@@ -69,6 +96,7 @@
       });
 
 
+  // Регистрирую кастомный элемент.
   document.registerElement('x-todo', { prototype: todoProto });
 
 })();
